119b93e1542fa998cac0df80a35f39076336d4fe
SharonMozes <sharonmozes24@gmail.com>
2025-04-05 00:17:51 +0530

    Home page

--- src/view/App/App.jsx
+++ src/view/App/App.jsx
@@ -1,67 +1,83 @@
-import Footer from "../../components/footer/footer"
-import Navbar from "../../components/navbar/nav"
-import Card3 from "../../components/homepageCard3/card3"
-import './App.css'
-import card3card from "./../../config/card3"
-import { Link } from "react-router-dom"
-import FruitsCard from "../../components/card4components/card4"
-import Section1 from "../../components/homepageCard3/card1"
-import {Toaster} from "react-hot-toast"
+import React, { useState } from "react";
+import Footer from "../../components/footer/footer";
+import Navbar from "../../components/navbar/nav";
+import Card3 from "../../components/homepageCard3/card3";
+import "./App.css";
+import card3card from "./../../config/card3";
+import { Link } from "react-router-dom";
+
+import { Toaster } from "react-hot-toast";
+
 function App() {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
 
   return (
     <>
-
       <Navbar />
-      <div className="App">
-      </div>
 
-      {/* card number  1 */}
+      {/* Scooter Background */}
+      <div className="App"></div>
 
-     <Section1/>
+      {/* Message after scooter disappears */}
+      <div className="scooter-message">Welcome to MediFind!!! 🚀</div>
+
+      
 
       <div className="why-choose-us-content">
-        <img src="https://cdn.shopify.com/s/files/1/0646/0568/3879/files/leaf.png?v=1719552788"
+        <img
+          src="https://cdn.shopify.com/s/files/1/0646/0568/3879/files/leaf.png?v=1719552788"
           alt="Leaf Icon"
-          className="leaf-icon left" />
+          className="leaf-icon left"
+        />
 
         <div className="why-choose-us-text">
-          <h1 className='why-choose-us-heading'>Why Choose Us ?</h1>
-          <p>By building a culture of sustainability and wholesome living, we strive for a healthier you and a healthier planet.
-      <Link to="/about" className="knowmore-button">know more</Link>
-
+          <h1 className="why-choose-us-heading">Why Choose Us?</h1>
+          <p>
+          MediFind ! your trusted path to better health,made simple and seamless
+            <Link to="/about" className="knowmore-button">Know more</Link>
           </p>
+
+          {/* Buttons Section */}
+          <div className="button-container">
+            <button className="shop-now-button">
+              <Link to="/services">Shop Now</Link>
+            </button>
+
+            {/* Upload Prescription Section */}
+            <label htmlFor="prescription-upload" className="upload-button">
+              Upload Prescription
+              <input
+                type="file"
+                id="prescription-upload"
+                accept="image/*,application/pdf"
+                onChange={handleFileChange}
+                style={{ display: "none" }}
+              />
+            </label>
+
+            {/* Display selected file name */}
+            {file && <p className="file-name">Selected: {file.name}</p>}
+          </div>
         </div>
 
-        <img src="https://cdn.shopify.com/s/files/1/0646/0568/3879/files/leaf.png?v=1719552788"
+        <img
+          src="https://cdn.shopify.com/s/files/1/0646/0568/3879/files/leaf.png?v=1719552788"
           alt="Leaf Icon"
-          className="leaf-icon right" />
-
+          className="leaf-icon right"
+        />
       </div>
 
-
-
-
-      <FruitsCard />
-      <br />
-      <div className="App-section-3-container">
-        {card3card.map((item, index) => (
-          <Card3
-            key={index}
-            image={item.image}
-            description={item.description}
-            Link1={item.link}
-
-          />
-        ))}
-
-      </div>
-
+      
+      
 
       <Footer />
-      <Toaster/>
+      <Toaster />
     </>
-
-  )
+  );
 }
-export default App
+
+export default App;
